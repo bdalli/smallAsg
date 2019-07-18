f0e5c5984eca532da9cab836eb2c7fcdf99f294d
bdalli <bdalli52@gmail.com>
2019-07-18 15:42:10 +0100

    ssh only

--- modules/mod-vpc/security_groups.tf
+++ modules/mod-vpc/security_groups.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "bastionSg" {
 
   ingress {
     from_port   = 22
-    to_port     = 80
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
