40c64030ae3068f357b46cb911d88f6a346c6d13
bdalli <bdalli52@gmail.com>
2019-07-18 15:51:58 +0100

    ssh only

--- modules/mod-vpc/security_groups.tf
+++ modules/mod-vpc/security_groups.tf
@@ -65,8 +65,8 @@ resource "aws_security_group" "smallAsgElbSg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 80
+    to_port     = 80
     protocol    = -1
     cidr_blocks = ["${var.vpc_cidr}"]
 
