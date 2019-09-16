323be5f42daf71e4aaf2e2428b05f4a636f980eb
bdalli <bdalli52@gmail.com>
2019-09-16 13:25:40 +0100

    packer role,user and policy

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -47,4 +47,85 @@ resource "aws_iam_user_policy" "tf_remote_policy" {
 }
 EOF
 }
+# iam for packer create user and role for policy attachment.
 
+resource "aws_iam_user" "packer_user" {
+  name = "packer_user"
+}
+
+resource "aws_iam_role" "ec2_packer_role" {
+  name = "ec2_packer_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+
+
+resource "aws_iam_policy" "packer-policy" {
+  name        = "packer_policy"
+  description = ""
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+      "Effect": "Allow",
+      "Action" : [
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CopyImage",
+        "ec2:CreateImage",
+        "ec2:CreateKeypair",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteKeypair",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteVolume",
+        "ec2:DeregisterImage",
+        "ec2:DescribeImageAttribute",
+        "ec2:DescribeImages",
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume",
+        "ec2:GetPasswordData",
+        "ec2:ModifyImageAttribute",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifySnapshotAttribute",
+        "ec2:RegisterImage",
+        "ec2:RunInstances",
+        "ec2:StopInstances",
+        "ec2:TerminateInstances"
+      ],
+      "Resource" : "*"
+  }]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "packer_policy_attach" {
+  name = "packer_policy_attach"
+
+  users      = ["${aws_iam_user.packer_user.name}"]
+  roles      = ["${aws_iam_role.ec2_packer_role.name}"]
+  policy_arn = "${aws_iam_policy.packer-policy.arn}"
+}
--- global/iam/outputs.tf
+++ global/iam/outputs.tf
@@ -5,3 +5,9 @@
 output "tf_remote_user_arn" {
   value = "${aws_iam_user.tf_remote.arn}"
 }
+
+output "packer_policy_arn" {
+  value = "${aws_iam_policy.packer-policy.arn}"
+
+}
+
